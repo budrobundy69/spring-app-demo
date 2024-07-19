4b45942a0254fd18c9f8a7470db05d4942a44c0b
Budro Bundy <budro.bundy69@gmail.com>
2024-07-19 18:19:23 +0200

    initial version

---
+++ src/test/java/com/example/springdemoapp/MyApplicationTest.java
@@ -0,0 +1,17 @@
+package com.example.springdemoapp;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MyApplicationTest {
+
+        @org.junit.jupiter.api.Test
+        void home() {
+            assertTrue(true);
+        }
+
+        @org.junit.jupiter.api.Test
+        void main() {
+            assertTrue(true);
+        }
+
+}
